d487c33f058f23e2e0f0ad54c0012764d72dbda0
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-01 05:23:48 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -35,10 +35,10 @@ class Transfer
     end  
   end 
   
-   def reject_transfer
-    self.status = "rejected"
-    "Transaction rejected. Please check your account #{balance}."
-  end
+  # def reject_transfer
+  #   self.status = "rejected"
+  #   "Transaction rejected. Please check your account #{balance}."
+  # end
 
 end
 v
