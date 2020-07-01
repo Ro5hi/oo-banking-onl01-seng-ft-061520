f4b675e88aaf2d12e89b4d5b105ec6075ced6b8e
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-01 05:26:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,14 +26,14 @@ class Transfer
   end
   
   def reverse_transfer
-      if valid? && receiver.balance > amount && self.status = "complete"
-      sender.balance += amount 
+  if valid? && receiver.balance > amount && self.status == "complete"
       receiver.balance -= amount
+      sender.balance += amount
       self.status = "reversed"
     else
       reject_transfer
-    end  
-  end 
+    end
+  end
   
   def reject_transfer
     self.status = "rejected"
